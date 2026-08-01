8219ef6e02c1f37d87ae94905ee474cdda97f068
Ricky200416 <rickypgk22@gmail.com>
2026-08-01 23:29:41 +0700

    Updates: 2026-08-01T16:29:37.309Z

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1417,6 +1417,8 @@
                          'icon'=>'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'],
                         ['route'=>'admin.kelola-website',       'label'=>'Kelola Website',   'match'=>'admin.kelola-website*',
                          'icon'=>'M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z'],
+                        ['route'=>'admin.alumni.index', 'label'=>'Data Alumni', 'match'=>'admin.alumni*',
+                         'icon'=>'M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422A12.083 12.083 0 0121 13c0 5.523-4.477 10-9 10S3 18.523 3 13c0-.538.04-1.066.118-1.578L12 14z'],
                     ];
                 @endphp
 

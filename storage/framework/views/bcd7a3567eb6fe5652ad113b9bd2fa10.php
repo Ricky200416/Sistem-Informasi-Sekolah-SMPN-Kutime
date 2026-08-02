b9127900f6de50375d1891da1640b6ef95d189d9
Ricky200416 <rickypgk22@gmail.com>
2026-08-02 15:34:09 +0700

    Updates: 2026-08-02T08:34:07.446Z

--- storage/framework/views/bcd7a3567eb6fe5652ad113b9bd2fa10.php
+++ storage/framework/views/bcd7a3567eb6fe5652ad113b9bd2fa10.php
@@ -698,7 +698,7 @@
                          'icon'=>'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1v-5m10-10l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-5'],
                         ['route'=>'admin.profil',               'label'=>'Data Diri',        'match'=>'admin.profil',
                          'icon'=>'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'],
-                        ['route'=>'admin.users',          'label'=>'Kelola User',      'match'=>'admin.users*',
+                        ['route'=>'admin.users*',          'label'=>'Kelola User',      'match'=>'admin.users*',
                          'icon'=>'M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2v-2a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2'],
                         ['route'=>'admin.kelas.index',          'label'=>'Kelola Kelas',     'match'=>'admin.kelas*',
                          'icon'=>'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4'],

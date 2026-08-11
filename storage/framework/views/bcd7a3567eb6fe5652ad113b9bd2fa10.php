a6ad660960da1e26049cd518331dc75c3ae79f29
Ricky200416 <rickypgk22@gmail.com>
2026-08-11 14:33:55 +0700

    Updates: 2026-08-11T07:33:52.540Z

--- storage/framework/views/bcd7a3567eb6fe5652ad113b9bd2fa10.php
+++ storage/framework/views/bcd7a3567eb6fe5652ad113b9bd2fa10.php
@@ -707,7 +707,7 @@
                          'icon'=>'M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'],
                         ['route'=>'admin.pengumuman',           'label'=>'Pengumuman',       'match'=>'admin.pengumuman*',
                          'icon'=>'M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'],
-                        ['route'=>'admin.absensi-guru.index',   'label'=>'Absensi Guru',     'match'=>'admin.absensi-guru*',
+                        ['route'=>'admin.absensi-guru.index',   'label'=>'Absensi Guru & Siswa',     'match'=>'admin.absensi-guru*',
                          'icon'=>'M8 7V3m8 4V3M5 11h14M5 19h14M5 5h2m10 0h2'],
                         ['route'=>'admin.perizinan.index',      'label'=>'Perizinan',        'match'=>'admin.perizinan*',
                          'icon'=>'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'],

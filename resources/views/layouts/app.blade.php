eeea6496520132436c7e357bc15ac14fc0b55a6d
Ricky200416 <rickypgk22@gmail.com>
2026-08-12 12:13:56 +0700

    Updates: 2026-08-12T05:13:50.325Z

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -789,6 +789,8 @@
                          'icon'=>'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4'],
                         ['route'=>'admin.alumni.index',         'label'=>'Alumni',           'match'=>'admin.alumni*',
                          'icon'=>'M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'],
+                        ['route'=>'admin.tenaga-pendidik.index', 'label'=>'Kelola Tenaga Pendidik', 'match'=>'admin.tenaga-pendidik*',
+                         'icon'=>'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z'],
                         ['route'=>'admin.pengumuman',           'label'=>'Pengumuman',       'match'=>'admin.pengumuman*',
                          'icon'=>'M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9',
                          'badge'=>$pengumumanBaruCount],
